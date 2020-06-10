07cb2beee1c1babb0e257371997745431ccc3044
João Palmeiro <joaommpalmeiro@gmail.com>
2020-06-10 19:07:43 +0100

    Added: Marimekko chart - first version.

--- altair-marimekko-chart/charts.py
+++ altair-marimekko-chart/charts.py
@@ -1,13 +1,37 @@
 import altair as alt
 
 
-def count_bar_chart(data, xvar):
+def count_bar_chart(data, xvar, w=300, h=300, bar_color="#cda279"):
     return (
-        alt.Chart(data)
-        .mark_bar()
+        alt.Chart(data, width=w, height=h)
+        .mark_bar(color=bar_color)
         .encode(
             x=alt.X(f"{xvar}:N"),
             y=alt.Y("count(*):Q", axis=alt.Axis(format="s", title="Count")),
-            tooltip=[f"{xvar}:N", alt.Tooltip("count(*):Q", title="Count")],
+            tooltip=[f"{xvar}:N", alt.Tooltip("count(*):Q", format=",", title="Count")],
         )
     )
+
+
+def marimekko_chart(data, xvar, yvar, w=400, h=400):
+    STROKE = 1
+
+    base = alt.Chart(data, width=w, height=h)
+
+    rect = base.mark_rect(
+        strokeWidth=STROKE,
+        stroke="white",
+        xOffset=STROKE / 2,
+        x2Offset=STROKE / 2,
+        yOffset=STROKE / 2,
+        y2Offset=STROKE / 2,
+    ).encode(
+        x=alt.X("x1:Q", axis=alt.Axis(zindex=1, format="%", title=xvar, grid=False)),
+        x2="x2:Q",
+        y=alt.Y("y1:Q", axis=alt.Axis(zindex=1, format="%", title=yvar, grid=False)),
+        y2="y2:Q",
+        color=alt.Color(f"{yvar}:N"),
+        tooltip=[alt.Tooltip(f"{xvar}:N"), alt.Tooltip(f"{yvar}:N")],
+    )
+
+    return rect.configure_view(strokeWidth=0)
--- altair-marimekko-chart/data.py
+++ altair-marimekko-chart/data.py
@@ -5,3 +5,30 @@ def load_mushroom_dataset(cols=[]):
     data = pd.read_csv("./raw_data/mushrooms.csv", usecols=cols if cols else None)
 
     return data
+
+
+def get_marimekko_data(data, xvar, yvar):
+    x_data = (
+        data[xvar]
+        .value_counts(normalize=True)
+        .to_frame(name=f"{xvar}_percentage")
+        .rename_axis(xvar)
+        .reset_index()
+    )
+
+    x_data["x2"] = x_data[f"{xvar}_percentage"].cumsum()
+    x_data["x1"] = x_data["x2"].shift(fill_value=0)
+
+    y_data = (
+        data.groupby(xvar)[yvar]
+        .value_counts(normalize=True)
+        .to_frame(name=f"{yvar}_percentage")
+        .reset_index()
+    )
+
+    y_data["y2"] = y_data.groupby(xvar)[f"{yvar}_percentage"].cumsum()
+    y_data["y1"] = y_data.groupby(xvar)["y2"].shift(fill_value=0)
+
+    marimekko_data = pd.merge(y_data, x_data, on=xvar)
+
+    return marimekko_data

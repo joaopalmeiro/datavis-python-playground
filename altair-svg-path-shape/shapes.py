e5453ae5a97c6ebe8a569cf5c297c46d0ca0de6a
João Palmeiro <joaommpalmeiro@gmail.com>
2020-07-23 17:51:09 +0100

    Added: Line cross shape.

--- altair-svg-path-shape/shapes.py
+++ altair-svg-path-shape/shapes.py
@@ -11,3 +11,4 @@ star = Shape(
     "M 0 0.6 L 0.6 0.9 L 0.5 0.2 L 1 -0.2 L 0.3 -0.3 L 0 -0.9 L -0.3 -0.3 L -1 -0.2 L -0.5 0.2 L -0.6 0.9 L 0 0.6 Z",
 )  # Based on: https://vega.github.io/vega/docs/marks/symbol/
 
+line_cross = Shape("Line Cross", "M 1 1 L -1 -1 M -1 1 L 1 -1")

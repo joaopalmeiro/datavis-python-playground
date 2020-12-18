d17a08aa3048f8294f548b1fc762548f938d73a1
João Palmeiro <joaommpalmeiro@gmail.com>
2020-12-18 23:33:01 +0000

    Update table

--- pandas-matplotlib-table/script.py
+++ pandas-matplotlib-table/script.py
@@ -1,10 +1,48 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 
-def render_mpl_table():
-    pass
+def render_mpl_table(
+    data,
+    col_width=3.0,
+    row_height=0.625,
+    bbox=[0, 0, 1, 1],
+    font_size=14,
+    edge_color="w",
+    header_color="#44475a",
+    row_colors=["#f5f5f5", "w"],
+    header_columns=0,
+    **kwargs,
+):
+    size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array(
+        [col_width, row_height]
+    )
+    fig, ax = plt.subplots(figsize=size)  # (width, height)
+    ax.axis("off")
+
+    mpl_table = ax.table(
+        cellText=data.values, bbox=bbox, colLabels=data.columns, **kwargs,
+    )
+
+    mpl_table.auto_set_font_size(False)
+    mpl_table.set_fontsize(font_size)
+
+    for k, cell in mpl_table._cells.items():
+        cell.set_edgecolor(edge_color)
+
+        if k[0] == 0 or k[1] < header_columns:
+            cell.set_text_props(weight="bold", color="w")
+            cell.set_facecolor(header_color)
+        else:
+            cell.set_facecolor(row_colors[k[0] % len(row_colors)])
+
+    return fig
 
 
 if __name__ == "__main__":
-    table = pd.DataFrame(np.random.randint(0, 100, size=(10, 5)), columns=list("ABCDE"))
+    data = pd.DataFrame(np.random.randint(0, 100, size=(20, 5)), columns=list("ABCDE"))
+
+    table = render_mpl_table(data)
+
+    table.savefig("table.png", dpi=300, bbox_inches="tight")

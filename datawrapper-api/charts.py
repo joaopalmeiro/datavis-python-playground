0e2bfc21cfdddd64e3c17c42e2c6e30247a39e26
João Palmeiro <joaommpalmeiro@gmail.com>
2021-01-05 03:18:41 +0000

    Update script and add pie chart for public communication

--- datawrapper-api/charts.py
+++ datawrapper-api/charts.py
@@ -1,4 +1,5 @@
 import os
+from functools import reduce
 from pathlib import Path
 
 import pandas as pd
@@ -7,6 +8,7 @@ from datawrapper import Datawrapper
 from dotenv import load_dotenv
 
 PIE_CHART = "d3-pies"
+SM_PIE_CHART = "d3-multiple-pies"
 VBAR_CHART = "column-chart"
 
 PURPLE = "#8624F5"
@@ -68,7 +70,9 @@ Datawrapper.export_chart = export_chart_patched
 
 def prepare_column(df, col, sort=True):
     if sort:
-        return df[col].value_counts().rename_axis(col).reset_index(name="count")
+        return (
+            df[col].value_counts(sort=sort).rename_axis(col).reset_index(name="count")
+        )
 
     return (
         df[col]
@@ -77,6 +81,15 @@ def prepare_column(df, col, sort=True):
         .rename_axis(col)
         .reset_index(name="count")
     )
+
+
+def prepare_multiple_columns(df, cols):
+    partial_dfs = [
+        df[col].value_counts().rename_axis("category").reset_index(name=col)
+        for col in cols
+    ]
+
+    return reduce(lambda x, y: pd.merge(x, y, on="category"), partial_dfs)
 
 
 if __name__ == "__main__":
@@ -88,98 +101,148 @@ if __name__ == "__main__":
     data = pd.read_csv("data/maja_horst_paper_table.csv")
 
     # Gender
-    # chart_info = dw.create_chart(
-    #     title="Gender of the interviewed scientists",
-    #     chart_type=PIE_CHART,
-    #     data=prepare_column(data, "Gender"),
-    # )
-    # dw.update_description(
-    #     chart_info["id"],
-    #     source_name="Horst, 2013",
-    #     source_url="https://doi.org/10.1177/1075547013487513",
-    # )
-    # properties = {
-    #     "visualize": {
-    #         "custom-colors": {"Male": GREEN, "Female": PURPLE},
-    #         "inside_labels": {"enabled": False},
-    #         "color_key": {
-    #             "stack": False,
-    #             "enabled": False,
-    #             "position": "top",
-    #             "label_values": True,
-    #         },
-    #         "pie_size": {"inside_labels": 50, "outside_labels": 25},
-    #         "outside_labels": {"edge": False, "color": True, "enabled": True},
-    #     }
-    # }
-    # dw.update_metadata(chart_info["id"], properties)
-    # dw.publish_chart(chart_info["id"], display=False)
-    # dw.export_chart(
-    #     chart_info["id"],
-    #     output="png",
-    #     filepath="img/gender.png",
-    #     display=False,
-    #     border_width=0,
-    #     zoom=10,
-    # )
+    chart_info = dw.create_chart(
+        title="Gender of the interviewed scientists",
+        chart_type=PIE_CHART,
+        data=prepare_column(data, "Gender"),
+    )
+    dw.update_description(
+        chart_info["id"],
+        source_name="Horst, 2013",
+        source_url="https://doi.org/10.1177/1075547013487513",
+    )
+    properties = {
+        "visualize": {
+            "custom-colors": {"Male": GREEN, "Female": PURPLE},
+            "inside_labels": {"enabled": False},
+            "color_key": {
+                "stack": False,
+                "enabled": False,
+                "position": "top",
+                "label_values": True,
+            },
+            "pie_size": {"inside_labels": 50, "outside_labels": 25},
+            "outside_labels": {"edge": False, "color": True, "enabled": True},
+        }
+    }
+    dw.update_metadata(chart_info["id"], properties)
+    dw.publish_chart(chart_info["id"], display=False)
+    dw.export_chart(
+        chart_info["id"],
+        output="png",
+        filepath="img/gender.png",
+        display=False,
+        border_width=0,
+        zoom=10,
+    )
 
     # Age group
-    # chart_info = dw.create_chart(
-    #     title="Age group of the interviewed scientists",
-    #     chart_type=VBAR_CHART,
-    #     data=prepare_column(data, "Age, years", sort=False),
-    # )
-    # dw.update_description(
-    #     chart_info["id"],
-    #     source_name="Horst, 2013",
-    #     source_url="https://doi.org/10.1177/1075547013487513",
-    # )
-    # properties = {
-    #     "visualize": {"rotate-labels": "off", "custom-colors": {"Undisclosed": GRAY},}
-    # }
-    # dw.update_metadata(chart_info["id"], properties)
-    # dw.publish_chart(chart_info["id"], display=False)
-    # dw.export_chart(
-    #     chart_info["id"],
-    #     output="png",
-    #     filepath="img/age_group.png",
-    #     display=False,
-    #     border_width=0,
-    #     zoom=8,
-    # )
+    chart_info = dw.create_chart(
+        title="Age group of the interviewed scientists",
+        chart_type=VBAR_CHART,
+        data=prepare_column(data, "Age, years", sort=False),
+    )
+    dw.update_description(
+        chart_info["id"],
+        source_name="Horst, 2013",
+        source_url="https://doi.org/10.1177/1075547013487513",
+    )
+    properties = {
+        "visualize": {"rotate-labels": "off", "custom-colors": {"Undisclosed": GRAY},}
+    }
+    dw.update_metadata(chart_info["id"], properties)
+    dw.publish_chart(chart_info["id"], display=False)
+    dw.export_chart(
+        chart_info["id"],
+        output="png",
+        filepath="img/age_group.png",
+        display=False,
+        border_width=0,
+        zoom=8,
+    )
 
     # Research field
-    # chart_info = dw.create_chart(
-    #     title="Research field of the interviewed scientists",
-    #     chart_type=PIE_CHART,
-    #     data=prepare_column(data, "Field of research"),
-    # )
-    # dw.update_description(
-    #     chart_info["id"],
-    #     source_name="Horst, 2013",
-    #     source_url="https://doi.org/10.1177/1075547013487513",
-    # )
-    # properties = {
-    #     "visualize": {
-    #         "custom-colors": {"Biotechnology": 0, "Nanotechnology": 1},
-    #         "inside_labels": {"enabled": False},
-    #         "color_key": {
-    #             "stack": False,
-    #             "enabled": False,
-    #             "position": "top",
-    #             "label_values": True,
-    #         },
-    #         "pie_size": {"inside_labels": 50, "outside_labels": 25},
-    #         "outside_labels": {"edge": False, "color": True, "enabled": True},
-    #     }
-    # }
-    # dw.update_metadata(chart_info["id"], properties)
-    # dw.publish_chart(chart_info["id"], display=False)
-    # dw.export_chart(
-    #     chart_info["id"],
-    #     output="png",
-    #     filepath="img/research_field.png",
-    #     display=False,
-    #     border_width=0,
-    #     zoom=10,
-    # )
+    chart_info = dw.create_chart(
+        title="Research field of the interviewed scientists",
+        chart_type=PIE_CHART,
+        data=prepare_column(data, "Field of research"),
+    )
+    dw.update_description(
+        chart_info["id"],
+        source_name="Horst, 2013",
+        source_url="https://doi.org/10.1177/1075547013487513",
+    )
+    properties = {
+        "visualize": {
+            "custom-colors": {"Biotechnology": 0, "Nanotechnology": 1},
+            "inside_labels": {"enabled": False},
+            "color_key": {
+                "stack": False,
+                "enabled": False,
+                "position": "top",
+                "label_values": True,
+            },
+            "pie_size": {"inside_labels": 50, "outside_labels": 25},
+            "outside_labels": {"edge": False, "color": True, "enabled": True},
+        }
+    }
+    dw.update_metadata(chart_info["id"], properties)
+    dw.publish_chart(chart_info["id"], display=False)
+    dw.export_chart(
+        chart_info["id"],
+        output="png",
+        filepath="img/research_field.png",
+        display=False,
+        border_width=0,
+        zoom=10,
+    )
+
+    # Media presence and policy-making engagement of the interviewed scientists
+    media_policy_data = prepare_multiple_columns(
+        data, ["Media presence", "Policy-making engagement"]
+    )
+
+    media_policy_data["category"] = media_policy_data["category"].astype("category")
+    media_policy_data["category"].cat.set_categories(
+        ["Low", "Medium", "High"], inplace=True
+    )
+    media_policy_data = media_policy_data.sort_values(by="category")
+
+    print(media_policy_data)
+
+    chart_info = dw.create_chart(
+        title="Level of public communication of the interviewed scientists",
+        chart_type=SM_PIE_CHART,
+        data=media_policy_data,
+    )
+    dw.update_description(
+        chart_info["id"],
+        source_name="Horst, 2013",
+        source_url="https://doi.org/10.1177/1075547013487513",
+    )
+    properties = {
+        "annotate": {
+            "notes": "This information is based on the public profiles collected for the article."
+        },
+        "visualize": {
+            "inside_labels": {"enabled": True},
+            "color_key": {
+                "stack": False,
+                "enabled": True,
+                "position": "top",
+                "label_values": False,
+            },
+            "pie_size": {"inside_labels": 50, "outside_labels": 50},
+            "slice_order": "original",
+        },
+    }
+    dw.update_metadata(chart_info["id"], properties)
+    dw.publish_chart(chart_info["id"], display=False)
+    dw.export_chart(
+        chart_info["id"],
+        output="png",
+        filepath="img/media_and_policy.png",
+        display=False,
+        border_width=0,
+        zoom=10,
+    )

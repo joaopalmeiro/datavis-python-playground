6cb9b56a050869aa1000b0832abdf6d35970a9be
João Palmeiro <joaommpalmeiro@gmail.com>
2021-01-14 23:11:16 +0000

    Update dataset

--- scoring-confusion-matrix/utils.py
+++ scoring-confusion-matrix/utils.py
@@ -1,21 +1,43 @@
 import os
+import random
+
+import numpy as np
+import pandas as pd
+from sklearn.datasets import make_classification
 
 
-def sow(seed=0):
+def sow(seed: int = 0) -> None:
     random.seed(seed)
 
     # More info: https://python.readthedocs.io/en/latest/using/cmdline.html#envvar-PYTHONHASHSEED
     os.environ["PYTHONHASHSEED"] = str(seed)
 
 
-def prepare_titanic_data(data):
-    data["sibsp_parch"] = data["SibSp"] + data["Parch"]
-    data["is_female"] = (data["Sex"] != "male").astype(int)
+def make_classification_df(
+    seed: int,
+    n_samples: int = 10_000,
+    n_features: int = 25,
+    n_informative: int = 10,
+    n_redundant: int = 10,
+    n_repeated: int = 5,
+    class_sep: float = 0.2,
+    flip_y: float = 0.1,
+    target_col: str = "target",
+) -> pd.DataFrame:
+    """Source of default values: https://queirozf.com/entries/scikit-learn-examples-making-dummy-dataset."""
+    X, y = make_classification(
+        n_samples=n_samples,
+        n_features=n_features,
+        n_informative=n_informative,
+        n_redundant=n_redundant,
+        n_repeated=n_repeated,
+        class_sep=class_sep,
+        flip_y=flip_y,
+        random_state=seed,
+    )
+    df = pd.DataFrame(np.c_[X, y])
 
-    median_age = data["Age"].dropna().median()
-    data["age_imputed"] = data["Age"].fillna(median_age)
+    df[df.columns[-1]] = df[df.columns[-1]].astype(int)
+    df = df.rename(columns={df.columns[-1]: target_col})
 
-    return data[
-        ["Survived", "Pclass", "is_female", "sibsp_parch", "Fare", "age_imputed"]
-    ]
-
+    return df

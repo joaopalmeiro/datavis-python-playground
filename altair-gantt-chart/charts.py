9b8b3a6d96afff9074e3dda78a3e5fe479ba2a48
João Palmeiro <joaommpalmeiro@gmail.com>
2021-05-03 14:23:02 +0100

    Update colors

--- altair-gantt-chart/charts.py
+++ altair-gantt-chart/charts.py
@@ -5,6 +5,11 @@ import pandas as pd
 
 from utils import compute_size_with_aspect_ratio, split_rows_with_marker
 
+# Colors:
+# - https://colors.eva.design/
+# - https://akveo.github.io/nebular/docs/design-system/dark-theme
+BLACK_COLOR: str = "#222b45"  # color-basic-800
+
 
 def gantt_chart(
     data: pd.DataFrame,
@@ -13,6 +18,8 @@ def gantt_chart(
     yvar: str,
     width: int = 500,
     marker: Optional[pd.DataFrame] = None,
+    color: str = "#3366ff",  # color-primary-500
+    deemphasis_color: str = "#a6c1ff",  # color-primary-300
 ) -> Union[alt.Chart, alt.LayerChart]:
     height = compute_size_with_aspect_ratio(width)
 
@@ -20,14 +27,30 @@ def gantt_chart(
 
     gantt = (
         alt.Chart(data, width=width, height=height)
-        .mark_bar(tooltip=False)
+        .mark_bar(color=color, tooltip=False)
         .encode(
-            x=alt.X(f"{xstart_var}:T", axis=alt.Axis(grid=False, title=None)),
+            x=alt.X(
+                f"{xstart_var}:T",
+                axis=alt.Axis(
+                    grid=False,
+                    title=None,
+                    tickColor=BLACK_COLOR,
+                    labelColor=BLACK_COLOR,
+                    domainColor=BLACK_COLOR,
+                ),
+            ),
             x2=alt.X2(f"{xend_var}:T"),
             y=alt.Y(
                 f"{yvar}:O",
                 sort=None,
-                axis=alt.Axis(grid=True, domain=False, ticks=True, title=None),
+                axis=alt.Axis(
+                    grid=True,
+                    domain=False,
+                    ticks=True,
+                    title=None,
+                    labelColor=BLACK_COLOR,
+                    gridColor="#e4e9f2",  # color-basic-400
+                ),
             ),
         )
     )
@@ -36,19 +59,23 @@ def gantt_chart(
         gantt = gantt.encode(
             color=alt.condition(
                 alt.datum[xend_var] <= alt.expr.time(marker["date"].item()),
-                alt.value("blue"),
-                alt.value("gray"),
+                alt.value(color),
+                alt.value(deemphasis_color),
             )
         )
 
         base_marker = alt.Chart(marker).encode(x="date:T")
 
         vrule = base_marker.mark_rule(
-            xOffset=0.5, strokeDash=[5, 1], tooltip=False
+            strokeDash=[5, 1], tooltip=False, color=color
         ).encode(size=alt.value(1.0))
 
         text = base_marker.mark_text(
-            align="center", baseline="bottom", dy=-height / 2 - 3, tooltip=False,
+            align="center",
+            baseline="bottom",
+            dy=-height / 2 - 3,
+            tooltip=False,
+            color=BLACK_COLOR,
         ).encode(text="message:N")
 
         gantt = alt.layer(gantt, vrule, text)
